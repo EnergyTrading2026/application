32dc02181eb09c50bed9d670026d0f49ee97272e
Maurice Tarp <maurice.tarp@outlook.de>
2026-05-17 18:11:01 +0200

    cleanup of project structure

---
+++ backend/src/main/java/xyz/energytrading/EnergyTradingApplication.java
@@ -0,0 +1,13 @@
+package xyz.energytrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EnergyTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyTradingApplication.class, args);
+	}
+}
